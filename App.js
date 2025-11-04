e31b95c13550c10c762dd8b068a7a2b62ef6a597
zhengye <958150192@qq.com>
2025-11-04 15:51:52 +0800

    test: AI-Codereview-Gitlab 03

--- App.js
+++ App.js
@@ -4,16 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   console.log('App 运行了!!!')
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={[{ fontSize: 50, width: 200 }, styles.title]}>
-          欢迎来到长乐未央 React Native 课程!
+      <View style={styles.container}>
+        <View>
+          <Text style={[{ fontSize: 50, width: 200 }, styles.title]}>
+            欢迎来到长乐未央 React Native 课程!
+          </Text>
+        </View>
+        <Text style={{ fontSize: 22, fontWeight: 'bold', color: '#ff7f6f' }}>
+          Hello React Native!
         </Text>
       </View>
-      <Text style={{ fontSize: 22, fontWeight: 'bold', color: '#ff7f6f' }}>
-        Hello React Native!
-      </Text>
-    </View>
   );
 }
 
