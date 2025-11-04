c0258d0c55af5cc5ab1acf5e6968528e9b526c28
zhengye <958150192@qq.com>
2025-11-04 16:44:28 +0800

    feat: 自定义加载中组件13

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import Loading from './components/shared/Loading';
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  console.log('App 运行了!!!')
+  const [courses, setCourses] = useState([]);
+  const [keyword, setKeyword] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  /**
+   * 获取搜索接口课程数据
+   * @returns {Promise<void>}
+   */
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `http://192.168.17.105:3000/search?q=${keyword}`
+      );
+      const { data } = await res.json();
+      setCourses(data.courses);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // 默认情况下，只要组件发生重新渲染，useEffect 就会再次执行。
+  useEffect(() => {
+    fetchData();
+  }, [keyword]); // 添加空依赖数组，确保只在组件挂载时执行一次
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-      <View style={styles.container}>
-        <View>
-          <Text style={[{ fontSize: 50, width: 200 }, styles.title]}>
-            欢迎来到长乐未央 React Native 课程!
-          </Text>
-        </View>
-        <Text style={{ fontSize: 22, fontWeight: 'bold', color: '#ff7f6f' }}>
-          Hello React Native!
+    <View style={styles.container}>
+      <Text>您搜索的关键词是：{keyword}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="请填写要所搜索的课程!"
+        onChangeText={text => setKeyword(text)}
+        defaultValue={keyword}
+      />
+
+      {courses.map((course) => (
+        <Text key={course.id}>
+          {course.name}
         </Text>
-      </View>
+      ))}
+    </View>
   );
 }
 
@@ -24,9 +59,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    color: '#e29447',
-  }
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
 });
